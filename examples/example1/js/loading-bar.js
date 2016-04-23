f573a939c5870fa55abb4a3b1f91efdb139c2f51
Dusan-Dimitric <dimitric_vaz@yahoo.com>
2016-04-23 12:40:09 +0200

    Add optional percentage displays and advance methos

--- examples/example1/js/loading-bar.js
+++ examples/example1/js/loading-bar.js
@@ -6,14 +6,21 @@
 	function LoadingBar(options) {
 		this.maxBars = options.maxBars || 12;
 		this.loadingBarHTML = '<div class="loading-bar">';
-		for (var i = 0; i < this.maxBars; i++)
+		for (let i = 0; i < this.maxBars; i++)
 			this.loadingBarHTML += '<div class="bar"></div>';
+		if (options.showPercentage === true)
+			this.loadingBarHTML += '<div class="percentage-container"><div id="percentage">0%</div></div>';
+		this.percentage = 0;
 		this.loadingBarHTML += '</div>';
+
 
 		this.loadingBars = {};
 		this.barsLoaded = 0;
 		this.loadIntoDiv(options.div);
+		if (options.showPercentage === true)
+			this.percentageDisplay = document.getElementById('percentage');
 		this.initializeStyle(options.style);
+		return this;
 	}
 
 	// Styles the widget
@@ -25,12 +32,15 @@
 			// set the bar color
 			this.widget.barFilledColor = style.barFilledColor || '#FE8301';
 			this.widget.barEmptyColor  = style.barEmptyColor  || '#050505';
+			// set the percentage text color
+			this.widget.percentageColor = style.percentageColor || '#FFFFFF';
 
 		} else { // the user didn't provide his own style
 			// default all colors
-			this.widget.barFilledColor = '#FE8301';
-			this.widget.barEmptyColor  = '#050505';
-			this.widget.containerColor = '#111111';
+			this.widget.barFilledColor  = '#FE8301';
+			this.widget.barEmptyColor   = '#050505';
+			this.widget.containerColor  = '#111111';
+			this.widget.percentageColor = '#FFFFFF';
 		}
 
 		this.applyStyles();
@@ -40,7 +50,8 @@
 	// Applies all the styles
 	LoadingBar.prototype.applyStyles = function() {
 		this.widget.style.backgroundColor = this.widget.containerColor;
-
+		if (this.percentageDisplay !== undefined)
+			this.percentageDisplay.style.color = this.widget.percentageColor;
 		this.resetBars();
 	};
 
@@ -76,22 +87,82 @@
 		var barsLoaded = this.barsLoaded;
 		var maxBars = this.maxBars;
 		var self = this;
+		var displayPercentage = (this.percentageDisplay !== undefined);
 		var barFilledColor = this.widget.barFilledColor;
 		setInterval(function() {
 			if (barsLoaded >= maxBars) {
 				barsLoaded = 0;
 				self.resetBars();
+				this.percentage = 0;
+				self.updatePercentage(barsLoaded);
 				return;
 			}
 			self.loadingBars[barsLoaded].style.backgroundColor = barFilledColor;
+
 			barsLoaded++;
+
+			if (displayPercentage)
+				self.updatePercentage(barsLoaded);
+			
 		}, speed || 100);
+	};
+
+	// Advance the loading by a given percentage
+	LoadingBar.prototype.advanceByPercentage = function(percent) {
+		if (this.barsLoaded <= this.maxBars) {
+			this.barsLoaded += ~~(this.maxBars * percent); // Using the bitwise NOT operator to convert to int
+
+			if (this.barsLoaded >= this.maxBars)
+				this.barsLoaded = this.maxBars;
+
+			// Fill affected bars
+			this.fillBars();
+
+			// Update the percentage display
+			if (this.percentageDisplay !== undefined)
+				this.updatePercentage(this.barsLoaded);
+		}
+		return this;
+	};
+
+	// Advance the loading by a given number of bars
+	LoadingBar.prototype.advanceBy = function(barsNumber) {
+		if (this.barsLoaded <= this.maxBars) {
+			this.barsLoaded += barsNumber;
+
+			if (this.barsLoaded >= this.maxBars)
+				this.barsLoaded = this.maxBars;
+
+			// Fill affected bars
+			this.fillBars();
+
+			// Update the percentage display
+			if (this.percentageDisplay !== undefined)
+				this.updatePercentage(this.barsLoaded);
+		}
+		return this;
+	};
+
+	LoadingBar.prototype.fillBars = function(percent) {
+		for (let i = 0; i < this.barsLoaded; i++) {
+			this.loadingBars[i].style.backgroundColor = this.widget.barFilledColor;
+		}
+	};
+	
+	// Update the percentage text
+	LoadingBar.prototype.updatePercentage = function(barsLoaded) {
+		if (barsLoaded > this.maxBars)
+			this.percentage = 0;
+		else
+			// Using the bitwise NOT operator to convert to int
+			this.percentage = ~~((barsLoaded / this.maxBars) * 100);
+		this.percentageDisplay.innerHTML = this.percentage + '%';
 	};
 
 	// Sets the colors of all the bars to black
 	LoadingBar.prototype.resetBars = function() {
 		var emptyColor = this.widget.barEmptyColor;
-		for (var i = 0; i < this.maxBars; i++) {
+		for (let i = 0; i < this.maxBars; i++) {
 			this.loadingBars[i].style.backgroundColor = emptyColor;
 		}
 	};
